3d52502fc7fd55c2e70599823b6f96d274b8b44e
Igor Kolomytsev <igor.kolomitseff@yandex.ru>
2024-03-01 16:28:52 +0300

    Update project

--- anfisa_for_friends/homepage/views.py
+++ anfisa_for_friends/homepage/views.py
@@ -1,6 +1,15 @@
 from django.shortcuts import render
 
+from ice_cream.models import IceCream
+
 
 def index(request):
-    template = 'homepage/index.html'
-    return render(request, template)
+    return render(request, 'homepage/index.html', {
+        'ice_cream_list': IceCream.objects.values(
+            'id', 'title', 'price', 'description'
+        ).filter(
+            is_published=True,
+            is_on_main=True,
+            category__is_published=True
+        )
+    })
--- anfisa_for_friends/ice_cream/views.py
+++ anfisa_for_friends/ice_cream/views.py
@@ -1,13 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
+
+from ice_cream.models import IceCream
 
 
 def ice_cream_detail(request, pk):
-    template = 'ice_cream/detail.html'
-    context = {}
-    return render(request, template, context)
+    return render(request, 'ice_cream/detail.html', {
+        'ice_cream': get_object_or_404(
+            IceCream.objects.filter(
+                is_published=True,
+                category__is_published=True
+            ),
+            pk=pk
+        )
+    })
 
 
 def ice_cream_list(request):
-    template = 'ice_cream/list.html'
-    context = {}
-    return render(request, template, context)
+    return render(request, 'ice_cream/list.html', {
+        'ice_cream_list': IceCream.objects.select_related(
+            'category'
+        ).filter(
+            is_published=True,
+            category__is_published=True
+        ).order_by('category')
+    })
